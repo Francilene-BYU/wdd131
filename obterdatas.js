c885f521cbea79a5bf1dc886ec63ed269b614cae
Francilene Ferreira <fferreira5@byupathway.edu>
2026-09-08 21:44:46 -0300

    obterdata.js

---
+++ obterdatas.js
@@ -0,0 +1,6 @@
+
+const anoAtual = new Date().getFullYear();
+document.querySelector("#anoAtual").textContent = anoAtual;
+
+const ultimaModificacao = document.lastModified;
+document.querySelector("#ultimaModificacao").textContent = ultimaModificacao;
