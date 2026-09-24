b6e534dd82da89bc4641bda93241d1477feb73ae
Francilene Ferreira <fferreira5@byupathway.edu>
2026-09-24 19:48:18 -0300

    S04 – Atividade de Aprendizagem: Imagens com Lazy Loading

---
+++ semana04/scripts/carregamentolento.js
@@ -0,0 +1,5 @@
+const anoAtual = new Date().getFullYear();
+document.querySelector("#anoAtual").textContent = anoAtual;
+
+const ultimaModificacao = document.lastModified;
+document.querySelector("#ultimaModificacao").textContent = ultimaModificacao;
