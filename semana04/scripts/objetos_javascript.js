bc6a2f8460d03cf145c7e9edc71e977055ec0d11
Francilene Ferreira <fferreira5@byupathway.edu>
2026-09-24 21:52:30 -0300

    S04 – Atividade de Aprendizagem: Objetos JavaScript

---
+++ semana04/scripts/objetos_javascript.js
@@ -0,0 +1,39 @@
+let umCurso = {
+    codigo: "WDD131",
+    nome: "Fundamentos da Web Dinâmica",
+    creditos: 2,
+    secoes: [
+        {
+            secao: "001",
+            matriculado: 95,
+            instrutor: "Roberto Diaz Rodrigues"
+        },
+        {
+            secao: "002",
+            matriculado: 80,
+            instrutor: "Sarah Gobble"
+        }
+    ]
+};
+
+function definirInformacoesCurso(curso) {
+    document.querySelector("#nomeCurso").innerHTML = `${curso.codigo} – ${curso.nome}`;
+}
+
+function exibirSecoes(curso) {
+    const tbody = document.querySelector("#secoes tbody");
+    let linhas = "";
+
+    for (const secao of curso.secoes) {
+        linhas += `<tr>
+            <td>${secao.secao}</td>
+            <td>${secao.matriculado}</td>
+            <td>${secao.instrutor}</td>
+        </tr>`;
+    }
+
+    tbody.innerHTML = linhas;
+}
+
+definirInformacoesCurso(umCurso);
+exibirSecoes(umCurso);
